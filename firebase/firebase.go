efee9019a658b3b3218109e074737327db13ec84
theoj246 <theoj246@gmail.com>
2024-03-28 21:41:00 +0900

    Edit log

--- firebase/firebase.go
+++ firebase/firebase.go
@@ -11,24 +11,8 @@ import (
 )
 
 func Init() *firebase.App {
-	// isDeployed := os.Getenv("IS_DEPLOYED")
-
-	// var opt option.ClientOption
-	// if isDeployed == "true" {
-	// 	opt = option.WithCredentialsJSON([]byte(os.Getenv("FIREBASE_SERVICE_ACCOUNT_JSON")))
-	// } else {
-	// }
-	// opt := option.WithCredentialsFile(os.Getenv("FIREBASE_SERVICE_ACCOUNT_JSON"))
-	// var serviceAccount map[string]interface{}
-	// jsonBytes := []byte(os.Getenv("FIREBASE_SERVICE_ACCOUNT"))
-	// err := json.Unmarshal(jsonBytes, &serviceAccount)
-
 	opt := option.WithCredentialsJSON([]byte(os.Getenv("FIREBASE_SERVICE_ACCOUNT")))
-	// opt := option.WithCredentialsJSON([]byte(os.Getenv("FIREBASE_SERVICE_ACCOUNT")))
-	logger.Info("opt: ", opt)
-	logger.Info("background: ", context.Background())
 	fb, err := firebase.NewApp(context.Background(), nil, opt)
-	logger.Info("fb: ", fb)
 	if err != nil {
 		logger.Error("Error initializing firebase: ", err)
 	}
@@ -37,15 +21,12 @@ func Init() *firebase.App {
 }
 
 func ValidateToken(fb *firebase.App, authToken string) (*auth.Token, error) {
-	logger.Info("background: ", context.Background())
 	client, err := fb.Auth(context.Background())
-	logger.Info("Client: ", client)
 	if err != nil {
 		logger.Error("client error: ", err)
 		return nil, err
 	}
 	token, err := client.VerifyIDToken(context.Background(), authToken)
-	logger.Info("Token: ", token)
 	if err != nil {
 		logger.Error("validation error: ", err)
 		return nil, err
